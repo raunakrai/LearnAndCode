01befd63a95e9ec30d87a9f2fed3c85643d5fce9
raunakrai <raunak.rai@intimetec.com>
2023-12-18 15:35:33 +0530

    updated problem statement 1

--- Assignment 2/ProblemStatement1.java
+++ Assignment 2/ProblemStatement1.java
@@ -1,29 +1,31 @@
-public class mathCalculatorAndStringManipulator 
+public class mathCalculatorAndStringManipulator
 {
-    int value;
-    double pointValue;
-    String text;
+    int operandFirst;
+    double operandSecond;
+    String operandThird;
 
-    public void calculateAndPrintSum(int oprand1, int oprand2) 
+    public void calculateSum(int oprand1, int oprand2) 
     {
         int sum = oprand1 + oprand2;
         System.out.println("Result: " + sum);
     }
 
-    public void convertAndPrintToUpperCase(String oprand) 
+    public void convertToUpperCase(String oprand) 
     {
-        text = oprand.toUpperCase();
-        System.out.println("Updated String: " + text);
+        operandThird = oprand.toUpperCase();
+        System.out.println("Updated String: " + operandThird);
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
+public class Main 
+{
+    public static void main(String[] args) 
+    {
         mathCalculatorAndStringManipulator obj = new mathCalculatorAndStringManipulator();
-        obj.value = 10;
-        obj.pointValue = 20.5;
-        obj.text = "hello";
-        obj.calculateAndPrintSum(obj.a, 5);
-        obj.convertAndPrintToUpperCase("world");
+        obj.operandFirst = 10;
+        obj.operandSecond = 20.5;
+        obj.operandThird = "hello";
+        obj.calculateSum(obj.operandFirst, 5);
+        obj.convertToUpperCase("world");
     }
 }
