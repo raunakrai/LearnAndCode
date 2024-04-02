49689c3e9664aca4aedebc7c2483bd1eb5fa96a9
raunakrai <raunak.rai@intimetec.com>
2024-04-02 11:42:04 +0530

    CorrectedIndentation

--- Assignment 5/SenerioOne.js
+++ Assignment 5/SenerioOne.js
@@ -1,32 +1,34 @@
 try 
-    {
-        function CarAccidentByWoman() 
-        {
-            console.log("Women did accident of her Car");
-        }
+{
+  function CarAccidentByWoman() 
+  {
+    console.log("Women did accident of her Car");
+  }
 
-        function shockedReactionByMan() 
-        {
-            console.log("Man is shocked after doing the accident and defending himself");
-        }
+  function shockedReactionByMan() 
+  {
+    console.log(
+      "Man is shocked after doing the accident and defending himself"
+    );
+  }
 
-        CarAccidentByWoman();
-        shockedReactionByMan();
-    }   catch (error) 
-        {
-            function BlameOfCarAccident(error) 
-            {
-            console.log("Cry and blame man for accident", error.message);
-            }
+  CarAccidentByWoman();
+  shockedReactionByMan();
+} catch (error) 
+{
+  function BlameOfCarAccident(error) 
+  {
+    console.log("Cry and blame man for accident", error.message);
+  }
 
-        function apologizeToWoman(error) 
-        {
-            console.log("Say sorry to Woman and pay money", error.message);
-        }
+  function apologizeToWoman(error) 
+  {
+    console.log("Say sorry to Woman and pay money", error.message);
+  }
 
-        BlameOfCarAccident(error);
-        apologizeToWoman(error);
-    } finally 
-    {
-    console.log("take the car to mechanic");
-    }
+  BlameOfCarAccident(error);
+  apologizeToWoman(error);
+} finally 
+{
+  console.log("take the car to mechanic");
+}
